df88f7c6b136046d8652d63835a36fac6f8a6f89
idgsnwebmaster <webmaster@idgsn.com>
2010-01-10 14:24:00 -0500

    * Deleted spec.pdf and updated terms and advertisers_nav

--- inc/advertisers_nav.php
+++ inc/advertisers_nav.php
@@ -2,6 +2,5 @@
 	<ul>
 		<li<?php if ($page == "index") { ?> class="active"<?php } ?>><a href="index.php">Advertisers</a></li>
 		<li<?php if ($page == "specs") { ?> class="active"<?php } ?>><a href="specs.php">Online Specifications</a></li>
-		<li><a href="../pdf/idg_technetwork_online_spec.pdf" target="_blank">Print Version</a></li>
 	</ul>
 </div>
